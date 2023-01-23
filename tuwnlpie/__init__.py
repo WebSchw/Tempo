21e518a48a8742386bf386d4f48a36882833d1e3
WebSchw <websterschwarz@posteo.de>
2023-01-23 19:38:20 +0100

    troll

--- tuwnlpie/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "0.0.1"
-
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logging.basicConfig(
-    format="%(asctime)s : %(module)s (%(lineno)s) - %(levelname)s - %(message)s"
-)
